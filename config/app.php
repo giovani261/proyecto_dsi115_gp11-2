50a8e0a6460850e37e2be662b3f25f18b65f0b2b
unknown <ac16082@ues.edu.sv>
2022-04-04 16:18:07 -0600

    Forzar uso de https para todas las rutas cuando se esta en produccion, archivo AppServiceProider

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://unidad-medica-humana.herokuapp.com/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL', 'https://unidad-medica-humana.herokuapp.com/'),
+    'asset_url' => env('ASSET_URL', null),
 
     /*
     |--------------------------------------------------------------------------
