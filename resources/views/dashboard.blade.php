76966aece3e1ba9ee2311982287706d118fed046
KathCardenas <57737671+KathCardenas@users.noreply.github.com>
2022-04-30 14:17:40 -0600

    kath
    agregando resources carpeta

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,201 @@
+@extends('layouts.plantilla')
+
+@section('contenido')
+<br>
+<div class="container-fluid containercardsdash">
+    @isset($name)
+        @foreach($name as $productname)
+            <h1>{{ $productname }},</h1>
+        @endforeach
+    @endisset   
+    <br>
+    <!-- Card Signos-->
+    <div class="card" style="max-width: 18rem;">
+        <div class="card-header text-primary"><center>Signos vitales</center></div>
+        <div class="card-body">
+        <a class="acards" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+            <div class="container">
+                <center>
+                    <div class="row">
+                        <div class="col my-auto">
+                            Registrar Signos Vitales
+                        </div>
+
+                        <div class="col-md-auto my-auto">
+                            <i class="fa-solid fa-heart-pulse fa-4x"></i>
+                        </div>
+                    </div>
+                </center>
+            </div>   
+        </a>
+        </div>
+    </div>
+    <!-- Card Generar receta-->
+    <div class="card" style="max-width: 18rem;">
+        <div class="card-header text-primary"><center>Receta</center></div>
+        <div class="card-body">
+        <a class="acards" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+            <div class="container">
+                <center>
+                    <div class="row">
+                        <div class="col my-auto">
+                            Registrar Signos Vitales
+                        </div>
+
+                        <div class="col-md-auto my-auto">
+                            <i class="fa-solid fa-heart-pulse fa-4x"></i>
+                        </div>
+                    </div>
+                </center>
+            </div>   
+        </a>
+        </div>
+    </div>
+    <!-- Card Signos-->
+    <div class="card" style="max-width: 18rem;">
+        <div class="card-header text-primary"><center>Signos vitales</center></div>
+        <div class="card-body">
+        <a class="acards" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+            <div class="container">
+                <center>
+                    <div class="row">
+                        <div class="col my-auto">
+                            Registrar Signos Vitales
+                        </div>
+
+                        <div class="col-md-auto my-auto">
+                            <i class="fa-solid fa-heart-pulse fa-4x"></i>
+                        </div>
+                    </div>
+                </center>
+            </div>   
+        </a>
+        </div>
+    </div>
+    <!-- Card Generar receta-->
+    <div class="card" style="max-width: 18rem;">
+        <div class="card-header text-primary"><center>Receta</center></div>
+        <div class="card-body">
+        <a class="acards" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+            <div class="container">
+                <center>
+                    <div class="row">
+                        <div class="col my-auto">
+                            Registrar Signos Vitales
+                        </div>
+
+                        <div class="col-md-auto my-auto">
+                            <i class="fa-solid fa-heart-pulse fa-4x"></i>
+                        </div>
+                    </div>
+                </center>
+            </div>   
+        </a>
+        </div>
+    </div>
+    <!-- Card Signos-->
+    <div class="card" style="max-width: 18rem;">
+        <div class="card-header text-primary"><center>Signos vitales</center></div>
+        <div class="card-body">
+        <a class="acards" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+            <div class="container">
+                <center>
+                    <div class="row">
+                        <div class="col my-auto">
+                            Registrar Signos Vitales
+                        </div>
+
+                        <div class="col-md-auto my-auto">
+                            <i class="fa-solid fa-heart-pulse fa-4x"></i>
+                        </div>
+                    </div>
+                </center>
+            </div>   
+        </a>
+        </div>
+    </div>
+    <!-- Card Generar receta-->
+    <div class="card" style="max-width: 18rem;">
+        <div class="card-header text-primary"><center>Receta</center></div>
+        <div class="card-body">
+        <a class="acards" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+            <div class="container">
+                <center>
+                    <div class="row">
+                        <div class="col my-auto">
+                            Registrar Signos Vitales
+                        </div>
+
+                        <div class="col-md-auto my-auto">
+                            <i class="fa-solid fa-heart-pulse fa-4x"></i>
+                        </div>
+                    </div>
+                </center>
+            </div>   
+        </a>
+        </div>
+    </div>
+    <!-- Modal Signos-->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">Registrar Signos Vitales</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+            </button>
+        </div>
+        <div class="modal-body">
+        <form method="POST" id="modalsignos">
+        @csrf
+        <label for="inputnombre">Nombre</label>
+        <input id="inputnombre" type="text" class="form-control" name="NombreSigno2">
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+            <button type="button" class="btn btn-primary" onclick="SignosVitales();">Guardar</button>
+        </div>
+        </form>
+        </div>
+    </div>
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+<script>
+            function SignosVitales(){
+                var inputsigno = document.getElementById("inputnombre");
+                var valnombresigno = inputsigno.value;
+                Swal.fire({
+                    icon: 'info',
+                    title: 'Confirmar.',
+                    text: '¡Desea continuar?',
+                    showCancelButton: true,
+                    confirmButtonText: 'Ok',
+                    }).then((result) => {
+                    if (result.isConfirmed) {
+                        $.ajax({
+                            url:"{{route('signos')}}",
+                            type:"POST",
+                            data:{
+                                'NombreSigno': valnombresigno,
+                                "_token": $("meta[name='csrf-token']").attr("content")
+                            },
+                            dataType:"json",
+                            success: function(test){
+                                inputsigno.value="";
+                                Swal.fire({
+                                    icon: 'success',
+                                    title: 'Hecho!.',
+                                    text: 'Se registraron correctamente los Signos Vitales ' +test.nombre,
+                                    confirmButtonText: 'Ok',
+                                    })
+                                }
+                            });
+                    } else if (result.isDismissed) {
+                        Swal.fire('No se registraron los signos vitales', '', 'info')
+                    }
+                    })
+            }
+</script>
+@endsection
---
+++ resources/views/layouts/plantilla.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Unidad clinica humana</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap sidebar)-->
+        <!-- <link href="{{ asset('../resources/css/sidebar.css') }}" rel="stylesheet" /> -->
+        <link href="{{ asset('css/sidebar.css') }}" rel="stylesheet" />
+        <!-- Css propio-->
+        <!-- <link href="{{ asset('../resources/css/estilos3.css') }}" rel="stylesheet" /> -->
+        <link href="{{ asset('css/estilos3.css') }}" rel="stylesheet" />
+        <!-- Fontawesome-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- Bootstrap css cdn -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap js cdn -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- jquery cdn -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <!-- sweet alert cdn -->
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    </head>
+    <body>
+        <div class="loading-container">
+            <div class="loading"></div>
+            <div class="loading-text">Cargando...</div>
+        </div>
+        <div class="d-flex relative" id="wrapper">
+            <!-- Sidebar-->
+            <div class="border-end bg-white" id="sidebar-wrapper">
+                <div class="sidebar-heading border-bottom bg-light">Img</div>
+                <div class="list-group list-group-flush">
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="/dashboard"><i class="fa-solid fa-gauge fa-xl"></i> Panel de control</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!"><i class="fa-solid fa-calendar-days fa-xl"></i> Agendar cita</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!"><i class="fa-solid fa-users fa-xl"></i> Gestion de usuarios</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!"><i class="fa-solid fa-hammer fa-xl"></i> Gestion de insumos</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!"><i class="fa-solid fa-briefcase-medical fa-xl"></i> <span class="txtmenu"> Gestion de medicamentos</span></a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!"><i class="fa-solid fa-clipboard-list fa-xl"></i> Gestion de proveedores</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!"><i class="fa-solid fa-chart-area fa-xl"></i> Generar reporte</a>
+                </div>
+            </div>
+            <!-- Page content wrapper-->
+            <div id="page-content-wrapper"> 
+                <!-- Top navigation-->
+                <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                    <div class="container-fluid">
+                        <button class="btn" id="sidebarToggle"><i class="fa-solid fa-bars fa-xl"></i></button>
+                        <a class="ahome" href="#!"><i class="fa-solid fa-user-doctor fa-xl"></i> MEDICINA Y CIRUGIA GASTROINTESTINAL</a> 
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa-solid fa-circle-user fa-xl"></i> {{ Auth::user()->name }}</a>
+                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        <i class="fa-solid fa-right-from-bracket"></i> {{ __('Cerrar Sesion') }}
+                                        </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                        <!-- <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="#!">Something else here</a> -->
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <!-- Page content-->
+                @yield('contenido')
+            </div> 
+        </div>
+
+        <footer id="footer" class="midnight-blue">
+            <br>
+            <div class="container">
+                <div class="col-sm-9">
+                    <div style="float:left; padding:0 30px 0 0" class="navbar-brand2">
+                    <a href="">
+                        <img src="https://www.ues.edu.sv/themes/UES-THEME/assets/images/ues_logo3.svg" alt="logo">
+                    </a>
+                    </div>
+                    <div>2021 Universidad de El Salvador. </br> 
+                        Ciudad Universitaria "Dr. Fabio Castillo Figueroa",</br>Final de Av.Mártires y Héroes del 30 julio, San Salvador, </br> El Salvador, América Central. Teléfonos: +(503) 2511-2000. Todos los derechos reservados.
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <ul class="pull-right">
+                        <li><a href="#inicio"><i class="fas fa-home"></i> Inicio</a></li>
+                        <li><a href="https://www.ues.edu.sv/blog/post/croquis-sede-central-"><i class="fas fa-map-marked-alt"></i> Mapa del Sitio</a></li>
+                        
+                    </ul>
+                </div>
+            </div>
+       </footer>
+
+        <script src="{{ asset('js/scripts2.js') }}"></script>
+        <!-- incluye el paquete, para mostar alertas desde los controladores -->
+        @include('sweetalert::alert')
+        @yield('scripts')
+    </body>
+</html>
