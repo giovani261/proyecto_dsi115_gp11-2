a4811e138f3897743ac6678685e5d0ffb64c60b4
KathCardenas <57737671+KathCardenas@users.noreply.github.com>
2022-04-30 14:19:20 -0600

    Kath
    agregando routes carpeta

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\SignosController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/laravel', function () {
+    return view('welcome');
+});
+
+Route::match(['get','post'],'/dashboard', [DashboardController::class,'dashboard'])->name('dashboard');
+
+Route::match(['post'],'/signos',[SignosController::class,'signos'])->name('signos');
+
+// Route::match(['get', 'post'], '/db', function () {
+//     //consulta
+//     $sql = 'SELECT * FROM products';
+//     $products = DB::select($sql);
+//     Alert::success('Success Title', 'Productos obtenidos de la bd correctamente');
+//     return $products;
+// })->name('db');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
