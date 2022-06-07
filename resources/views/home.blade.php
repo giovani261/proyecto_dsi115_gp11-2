63a13f553af1d24275dbf853f367f8c00dc9c4a3
unknown <jc.1994dlc@gmail.com>
2022-06-06 22:52:42 -0600

    ultimos cambios-servicios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
     <!-- Favicon.ico -->
     <link rel="shortcut icon" type="image/x-icon" href="imgs/logo.jpeg">
     <!-- Favicon.ico -->
+
     <!-- CSS -->
     <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> este archivo es bootstrap 4, genera conflicto con los modal y la plantilla que se extiende debido a que usa bootstrap 5-->
     <link rel="stylesheet" href="css/owl.carousel.min.css">
@@ -26,6 +27,8 @@
     <link rel="stylesheet" href="css/gijgo.css">
     <link rel="stylesheet" href="css/animate.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/estilos4.css">
+
     <!-- <link rel="stylesheet" href="css/responsive.css"> -->
 </head>
 <body>
@@ -65,6 +68,60 @@
             </div>
         </div>
     </div>
+
+     <!--slider-->
+     <div id="slider">
+        <input type="radio" name="slider" id="slide1" checked>
+        <input type="radio" name="slider" id="slide2">
+        <input type="radio" name="slider" id="slide3">
+        <input type="radio" name="slider" id="slide4">
+        <div id="slides">
+           <div id="overflow">
+              <div class="inner">
+                 <div class="slide slide_1">
+                    <div class="slide-content">
+                        <img src="imgs/1.jpg" alt="">
+                        <div class="texto">
+                            <h5 style="color: black; line-height: 1.5em;">En la actualidad  las enfermedades gastrointestinales son muy frecuentes.<br>
+                            Muchas de ellas son graves, pueden ser tratadas exitosamente si su <br> 
+                            diagnóstico es oportuno y adecuado.</h5></div>
+                    </div>
+                 </div>
+                 <div class="slide slide_2">
+                    <div class="slide-content">
+                        <img src="imgs/2.jpg" alt="">
+                       <p>Content for Slide 2</p>
+                    </div>
+                 </div>
+                 <div class="slide slide_3">
+                    <div class="slide-content">
+                        <img src="imgs/3.jpg" alt="">
+                       <p>Content for Slide 3</p>
+                    </div>
+                 </div>
+                 <div class="slide slide_4">
+                    <div class="slide-content">
+                        <img src="imgs/4.jpg" alt="">
+                       <p>Content for Slide 4</p>
+                    </div>
+                 </div>
+              </div>
+           </div>
+        </div>
+        <div id="controls">
+           <label for="slide1"></label>
+           <label for="slide2"></label>
+           <label for="slide3"></label>
+           <label for="slide4"></label>
+        </div>
+        <div id="bullets">
+           <label for="slide1"></label>
+           <label for="slide2"></label>
+           <label for="slide3"></label>
+           <label for="slide4"></label>
+        </div>
+     </div>
+     <br>
     <!-- service_area_start -->
     <div class="service_area">
         <div class="container p-0">
@@ -75,7 +132,7 @@
                             <i class="flaticon-electrocardiogram"></i>
                         </div>
                         <h3>Hospitalidad</h3>
-                        <p>La excelencia clínica debe ser la prioridad para cualquier proveedor de servicios de atención médica.</p>
+                        <p>La excelencia clínica debe ser la prioridad para cualquier proveedor de servicios de atención m&#233;dica.</p>
                     </div>
                 </div>
                 <div class="col-xl-4 col-md-4">
@@ -84,7 +141,7 @@
                             <i class="flaticon-emergency-call"></i>
                         </div>
                         <h3>Cuidados de emergencia</h3>
-                        <p>La excelencia clínica debe ser la prioridad para cualquier proveedor de servicios de atención médica.</p>
+                        <p>Dispuestos a brindarte una experiencia poxitiva desde el inicio, contando con el equipo necesario.</p>
                     </div>
                 </div>
                 <div class="col-xl-4 col-md-4">
@@ -92,8 +149,8 @@
                         <div class="icon">
                             <i class="flaticon-first-aid-kit"></i>
                         </div>
-                        <h3>poner algo mas</h3>
-                        <p>La excelencia clínica debe ser la prioridad para cualquier proveedor de servicios de atención médica.</p>
+                        <h3>Servicio m&#233;dicos de excelencia</h3>
+                        <p>Servicios m&#233;dicos con excelente tenci&#243;n accesible, eficiente pero sobretodo, humana.</p>
                     </div>
                 </div>
             </div>
@@ -119,8 +176,11 @@
                             <img src="imgs/department/1.jpg" alt="">
                         </div>
                         <div class="department_content">
-                            <h3><a href="#" style="text-decoration:none;">Gastritis y Cáncer del Estomago</a></h3>
-                            <p>Esteem spirit temper too say adieus who direct esteem.</p>
+                            <h3><a href="#" style="text-decoration:none;">Gastritis y C&#225;ncer del Est&#243;mago</a></h3>
+                            <p>La gastritis es un t&#233;rmino para un grupo de enfermedades con un punto en com&#250;n: 
+                                la inflamaci&#243;n del revestimiento del est&#243;mago. El c&#225;ncer de est&#243;mago, es una 
+                                enfermedad por la que se forman c&#233;lulas malignas en el revestimiento del est&#243;mago
+                                </p>
                             <center>
                                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#citaModal" onclick="especialidad('gastritis');">Agendar cita</button>
                             </center>
@@ -133,8 +193,11 @@
                             <img src="imgs/department/2.jpg" alt="">
                         </div>
                         <div class="department_content">
-                            <h3><a href="#" style="text-decoration:none;">Colitis y cáncer de colon</a></h3>
-                            <p>Esteem spirit temper too say adieus who direct esteem.</p>
+                            <h3><a href="#" style="text-decoration:none;">Colitis y c&#225;ncer de colon</a></h3>
+                            <p>La colitis ulcerativa es una enfermedad que causa inflamaci&#243;n y &#250;lceras.
+                            El c&#225;ncer de colon es una enfermedad por la que se forman c&#233;lulas malignas en los tejidos del colon.
+                            </p>
+                            <br><br>
                             <center>
                                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#citaModal" onclick="especialidad('colitis');">Agendar cita</button>
                             </center>
@@ -148,7 +211,9 @@
                         </div>
                         <div class="department_content">
                             <h3><a href="#" style="text-decoration:none;">Estreñimiento y sangrado rectal</a></h3>
-                            <p>Esteem spirit temper too say adieus who direct esteem.</p>
+                            <p>El estreñimiento se caracteriza por las deposiciones poco frecuentes o la dificultad para evacuar.
+                            El sangrado rectal leve tiene lugar cuando se refiere a la eliminaci&#243;n de unas pocas gotas de sangre de color rojo
+                            </p>
                             <center>
                                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#citaModal" onclick="especialidad('estreñimiento');">Agendar cita</button>
                             </center>
@@ -162,7 +227,9 @@
                         </div>
                         <div class="department_content">
                             <h3><a href="#" style="text-decoration:none;">Hemorroides</a></h3>
-                            <p>Esteem spirit temper too say adieus who direct esteem.</p>
+                            <p>Las hemorroides o almorranas son un conjunto de tejidos inflamados en la zona anal. Contienen vasos sangu&#237;neos,
+                                 tejido conjuntivo, m&#250;sculo y fibras el&#225;sticas.</p>
+                            <br>
                             <center>
                                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#citaModal" onclick="especialidad('hemorroides');">Agendar cita</button>
                             </center>
@@ -175,8 +242,10 @@
                             <img src="imgs/department/5.jpg" alt="">
                         </div>
                         <div class="department_content">
-                            <h3><a href="#" style="text-decoration:none;">Cáncer recto y ano</a></h3>
-                            <p>Esteem spirit temper too say adieus who direct esteem.</p>
+                            <h3><a href="#" style="text-decoration:none;">C&#225;ncer recto y ano</a></h3>
+                            <p>El c&#225;ncer del recto es un c&#225;ncer que comienza en el recto. El recto consiste en los &#250;ltimos 
+                                cent&#237;metros del intestino grueso. El cáncer de ano es una enfermedad por la que se forman c&#233;lulas 
+                                malignas en los tejidos del ano</p>
                             <center>
                                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#citaModal" onclick="especialidad('cancer');">Agendar cita</button>
                             </center>
@@ -189,8 +258,10 @@
                             <img src="imgs/department/6.jpg" alt="">
                         </div>
                         <div class="department_content">
-                            <h3><a href="#" style="text-decoration:none;">Hígado y cálculos en vesícula</a></h3>
-                            <p>Esteem spirit temper too say adieus who direct esteem.</p>
+                            <h3><a href="#" style="text-decoration:none;">H&#237;gado y cálculos en ves&#237;cula</a></h3>
+                            <p>Los c&#225;lculos biliares son dep&#243;sitos endurecidos de fluido digestivo que se pueden 
+                            formar en la ves&#237;cula biliar.</p>
+                             <br><br>
                             <center>
                                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#citaModal" onclick="especialidad('higado');">Agendar cita</button>
                             </center>
@@ -203,8 +274,9 @@
                             <img src="imgs/department/7.jpg" alt="">
                         </div>
                         <div class="department_content">
-                            <h3><a href="#" style="text-decoration:none;">Reflujo gastro-esofágico</a></h3>
-                            <p>Esteem spirit temper too say adieus who direct esteem.</p>
+                            <h3><a href="#" style="text-decoration:none;">Reflujo gastro-esof&#225;gico</a></h3>
+                            <p>La enfermedad por reflujo gastroesof&#225;gico es una afecci&#243;n en la cual los contenidos estomacales se devuelven 
+                            desde el est&#243;mago hacia el esófago. Los alimentos van desde la boca hasta el est&#243;mago a través del es&#243;fago</p>
                             <center>
                                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#citaModal" onclick="especialidad('reflujo');">Agendar cita</button>
                             </center>
@@ -214,7 +286,7 @@
             </div>
         </div>
     </div>
-
+    <!-- offers_area_end -->
 <!-- Modal Agendar Cita-->
 <div class="modal fade" id="citaModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
     <div class="modal-dialog modal-dialog-centered">
@@ -250,7 +322,7 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="submit" class="btn btn-primary" id="guardar"><i class="fa-solid fa-floppy-disk"></i> Guardar</button>
+                <button type="submit" class="btn btn-primary" id="guardar">Guardar</button>
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onclick="removevalidateform('reservarCita');">Cancelar</button>
             </div></form>  
         </div>
@@ -268,18 +340,18 @@
                         <ul class="nav" id="myTab" role="tablist">
                             <li class="nav-item">
                                 <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home"
-                            aria-selected="true">Excelentes Servicios</a>
+                            aria-selected="true" style="text-shadow: 5px 5px 5px #de1a22; font-size: 24px;">Excelentes Servicios.</a>
                             </li>
 
                             <li class="nav-item">
                                 <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile"
-                            aria-selected="false">Medico calificado</a>
+                            aria-selected="false" style="text-shadow: 5px 5px 5px #de1a22; font-size: 24px;">Medico calificado.</a>
                             </li>
 
 
                             <li class="nav-item">
                                 <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact"
-                            aria-selected="false">Equipo necesario</a>
+                            aria-selected="false" style="text-shadow: 5px 5px 5px #de1a22; font-size: 23px;">Equipo moderno.</a>
                             </li>
                         </ul>
                     </div>
@@ -300,10 +372,11 @@
                                                         <i class="flaticon-first-aid-kit"></i>
                                                     </div>
                                                         <div class="department_content">
-                                                            <h3>Excelente servicio m&#233;dico</h3>
-                                                            <p style="font-size: 15px;"> &#34;Medicina y Cirug&#237;a Gastrointestinal J.V.P.M. 1668&#34; es una clinica
-                                                                que atiende enfermedades gastrointestinales. Ofrecemos servicios m&#233;dicos de
-                                                                excelencia, eficiente y accesibles.<br><br>
+                                                            <h3>Unidad M&#233;dica Humana.</h3>
+                                                            <p style="font-size: 15px;"> Ofrecemos consulta para el diagn&#243;stico y prevenci&#243;n de las enfermedades
+                                                                del aparato digestivo.<br><br>
+                                                                Dr. Ren&#233; Rodr&#237;guez es un m&#233;dico capacitado con experiencia en el manejo de enfermedades del tracto
+                                                                gastrointestinal y el h&#237;gado.
                                                                 Cualquier tipo de informaci&#243;n adional, con gusto le corresponderemos.
                                                                 Haga su cita o contactemos en nuestras redes sociales.                
                                                             </p>
@@ -315,7 +388,7 @@
                                             </div>
                                             <div class="col-xl-6 col-md-6">
                                                 <div class="business_thumb">
-                                                    <img src="imgs/about/business.png" alt="">
+                                                    <img src="imgs/about/business.jpg" alt="" style="width: 80%; height: 20%;">
                                                 </div>
                                             </div>
                                     </div>
@@ -328,7 +401,6 @@
     <!-- JS here -->
     <script src="js/vendor/modernizr-3.5.0.min.js"></script>
     <!--<script src="js/vendor/jquery-1.12.4.min.js"></script>-->
-
     <script src="js/popper.min.js"></script>
     <!-- <script src="js/bootstrap.min.js"></script> -->
     <script src="js/owl.carousel.min.js"></script>
@@ -345,7 +417,6 @@
     <script src="js/jquery.magnific-popup.min.js"></script>
     <script src="js/plugins.js"></script>
     <script src="js/gijgo.min.js"></script>
-
     <!--contact js-->
     <script src="js/contact.js"></script>
     <script src="js/jquery.ajaxchimp.min.js"></script>
@@ -353,6 +424,8 @@
     <script src="js/jquery.validate.min.js"></script>
     <script src="js/mail-script.js"></script>
     <script src="js/main.js"></script>
+    <!--slider-->
+
 </body>
 </html>
 
@@ -362,7 +435,7 @@
 <script src="{{ asset('js/bootstrap-datepicker.es.js') }}"></script>
 <script>
     $(document).ready(function() {
-        var datesForDisable = ["2021-06-03", "08-10-2021", "08-15-2021", "08-20-2021"]; 
+        var datesForDisable = ["2022-06-03", "08-10-2021", "08-15-2021", "08-20-2021"]; 
             $('#inputfecha').datepicker({
                 isRTL: false,
                 format: 'yyyy-mm-dd',
